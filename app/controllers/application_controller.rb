133fd32fef96a1c759519b1dd441266767db8455
Andy Chan <andy.chan9930@gmail.com>
2022-03-07 12:46:57 -0500

    commit tree 61ca5886fcd5d8d74ab18f875b2ca773fd390253

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,30 @@
 class ApplicationController < Sinatra::Base
+    # add routes
+  set :default_content_type, 'application/json'
 
-  # add routes
+  get '/bakeries' do
+    # get all the bakeries from the database
+    bakeries = Bakery.all
+    # send them back as a JSON array
+    bakeries.to_json
+  end
 
+  get '/bakeries/:id' do
+    # look up the game in the database using its ID
+    bakeries = Bakery.find params[:id]
+    # send a JSON-formatted response of the game data
+    bakeries.to_json include: :baked_goods
+  end
+
+  get '/baked_goods/by_price' do
+    #sort itemd in database by price
+    baked_goods = BakedGood.all.order price: :desc
+    #return as json
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    baked_goods = BakedGood.all.order price: :desc
+    baked_goods.first.to_json
+  end
 end
